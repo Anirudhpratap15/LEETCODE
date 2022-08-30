77fd930090351122f6e9191b7687ef26be65a0c3
Anirudhpratap15 <singh.anirudh15@gmail.com>
2022-08-30 15:13:28 +0530

    Time: 10 ms (70.38%), Space: 58.1 MB (25.87%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -13,8 +13,8 @@ class Solution {
             }
             if (matrix[r][c] < target) {
                 r = r + 1;
-                if(r<matrix.length){
-                c= matrix[r].length-1;}
+                // if(r<matrix.length){
+                // c= matrix[r].length-1;}
             } else c = c - 1;
 
 
