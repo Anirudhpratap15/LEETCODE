125f07767e51836451340a6d220811ec16ef882a
Anirudhpratap15 <singh.anirudh15@gmail.com>
2022-09-12 01:25:51 +0530

    Time: 1 ms (87.48%), Space: 44.2 MB (23.11%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -2,39 +2,38 @@ class Solution {
     public boolean search(int[] nums, int target) {
         
         int start = 0 ;
-        int end = nums.length-1;
+        int end  = nums.length-1;
         
-        while(end>=start){
+        while(end >=start){
             
-            int mid  = start +(end-start)/2 ; 
+            int mid  = start +(end - start)/2;
             
-            if(nums[mid] == target){
-                return true ; 
+            if(nums[mid]==target){
+                return true ;
             }
-             else if (nums[start] ==nums[mid]&&nums[end] ==nums[mid]){
-                end = end - 1 ;
-                 start = start +1 ;
+            
+            if(nums[start] == nums[mid]&&nums[end] == nums[mid]){
+                ++start;
+                --end ;
             }
-            //array is sorted from start to mid 
+            //if array is sorted from start to end 
             else if(nums[start]<=nums[mid]){
+                if(target>=nums[start]&&target<=nums[mid]){
+                    end = mid -1;
+                }
+                else start = mid +1 ;
                 
-               if(nums[start]<=target&& target<=nums[mid]){
-                    end = mid - 1 ;
-               }
-                 else start = mid+1 ;
             }
-        // If arr[l..mid] first subarray is not sorted
-        // then arr[mid... h] must be sorted subarray
-            else {
-                if(nums[end]>=target&& target>=nums[mid]){
-                start = mid +1;
-            }
-                  else end = mid -1;
+            // array is sorted from mid to end 
+            else{
+                if(target >=nums[mid]&&target<=nums[end]){
+                    start = mid +1 ;
+                }
+                else end = mid -1;
+            } 
+                
         }
-       
-         
-      
+        
+        return false ;
     }
-     return false  ;
-    
-    }}
+}
