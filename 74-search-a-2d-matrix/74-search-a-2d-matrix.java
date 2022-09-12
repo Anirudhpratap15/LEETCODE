266e28c99c36e5d19db6f6bee27207e9ea4cdf8a
Anirudhpratap15 <singh.anirudh15@gmail.com>
2022-09-12 23:13:08 +0530

    Time: 1 ms (38.28%), Space: 42.8 MB (44.07%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -11,8 +11,6 @@ class Solution {
             }
             if(matrix[r][c]<target){
               r= r+1 ; 
-                // if(r<matrix.length){
-                //     c= matrix[r].length-1;
                 }
             
             else c = c-1;
