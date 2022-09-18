f628b29339a7a60fe6e5f062188b87177166a7be
Anirudhpratap15 <singh.anirudh15@gmail.com>
2022-09-19 01:11:50 +0530

    Time: 3 ms (46.05%), Space: 55.7 MB (5.86%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,13 +1,21 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        //if length is  0 
         if(nums.length == 0 ) return ;
         
+        // starting from index  0 
         int  pos =  0 ; 
+        
+        
+        //taking all non zero element together in starting 
        for (int i = 0 ;i<nums.length;i++) {
+           // for non zero move to starting pos which is  zero in start 
         if (nums[i]!= 0){ 
             nums[pos++] = nums[i];}
        }
             
+        
+        //placing 0 in remaing index from position which we get by filling non zero 
             while(pos<nums.length){
                 nums[pos++] = 0 ;
             }
