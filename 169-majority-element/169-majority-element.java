5d5f4c1774adc718aba7ae851bac35d15e759941
Anirudh Pratap <singh.anirudh15@gmail.com>
2022-09-21 23:50:38 +0530

    Time: 2315 ms (5.00%), Space: 56.6 MB (24.20%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,8 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        //sorting array 
-        Arrays.sort(nums); 
-        // ans will be in n/2 as mention in question that element that appears more than ⌊n/2⌋ times. 
-       int ans =  nums[nums.length/2];
-        return ans ; 
+        int tempCount = nums.length/2;
+        
+        for(int i = 0 ; i <nums.length ; i++){
+            int count  = 0 ;
+            for(int j = 0 ; j <nums.length ; j++){
+                if(nums[i]==nums[j]){
+                    count+=1;
+                }
+            }
+            if(count>tempCount){
+                    return nums[i];
+                }
+            
+        } 
+        return -1 ; 
 }}
