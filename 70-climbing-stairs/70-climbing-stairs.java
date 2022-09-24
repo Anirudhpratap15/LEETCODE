9ea3153c8f9da29807687d4f381a2b620d925530
Anirudh Pratap <singh.anirudh15@gmail.com>
2022-09-24 15:06:42 +0530

    Time: 0 ms (100.00%), Space: 41 MB (40.76%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,8 +1,11 @@
 class Solution {
     public int climbStairs(int n) {
+        //take new array to store the ans to the corresponding nth value 
         int[] dp = new int[46];
+        
         dp[0]= 0 ;
         dp[1]= 1 ;
+        //there is two ways to climbing a staircase of 2  eg 1+1 ans only 2 
          dp[2]= 2 ;
         
         for(int i = 3;i<=n;i++){
