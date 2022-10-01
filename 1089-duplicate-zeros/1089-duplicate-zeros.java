62f5b36478132980e80a24062efc25d00801a3e8
Anirudh Pratap <singh.anirudh15@gmail.com>
2022-10-01 22:50:07 +0530

    Time: 22 ms (18.30%), Space: 45.3 MB (80.53%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,10 +1,11 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        
+        // check for 0 if found move all next vales to 1 step forward 
      for(int i = 0 ; i<arr.length;i++){
 
             if(arr[i]==0){
                 int j=arr.length-2;
+                //starting from seconf last  moving values forward
                 while(j>=i){
                     arr[j+1]= arr[j];
                     j--;
