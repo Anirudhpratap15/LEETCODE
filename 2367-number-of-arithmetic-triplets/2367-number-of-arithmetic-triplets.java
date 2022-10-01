abbbf0654adea6ddb43bb4d6d220e0aba3a67ea5
Anirudh Pratap <singh.anirudh15@gmail.com>
2022-10-01 22:01:10 +0530

    Time: 4 ms (51.74%), Space: 42.3 MB (34.53%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.java
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.java
@@ -1,42 +1,25 @@
 class Solution {
     public int arithmeticTriplets(int[] nums, int diff) {
+    
+        // taking hashMap to avoid duplicated 
+        HashMap<Integer,Integer> hashMap=new HashMap<>();
         int count = 0 ;
+      
+        //storing values in hasmMap
+        for(int i = 0 ;i<nums.length;i++){
+            hashMap.put(nums[i],1);}
         
-        // using two pointer
-        // loop for fixed element 
-        for(int i = 0 ;i<nums.length-2;i++){
-            //left pointer start from fixed element +1 ;
-            int left= i+1 ; 
-             //right pointer start from end ;
-            int right  = nums.length-1;
-           
-            //condition 
-            while(left<right){
-                // j-i
-                int c = nums[left] - nums[i] ; 
-                //k-j
-                 int d = nums[right] - nums[left] ;
-               
-                if(c ==diff&&d==diff)  {
-                    count +=1;
-          
-                    //when triplet found to remove dublicates check next values if same skip from left
-             while(left<right&&nums[left]==nums[left+1]) left++ ;
-                    //when triplet found to remove dublicates check next values if same skip from right
-            while(left<right&&nums[right]==nums[right-1]) right-- ;
-                                 
-                     left++;
-                     right--;
-                                 }
-                // if j-i is smaller than diff as array is sorted we need to move forward to increase the j-i value 
-                else if(c<diff) left++;
-                 // if k-j is smaller than diff as array is sorted we need to move forward to increase the k-j value 
-                 else if(d<diff) left++;
-                //when j-i and  k-j  is greater  move backward
-                else right-- ;
-            }
-        }
+        //checking hapMap now 
+        //if i+ diff is present then check for i +2*diff why ?  acc to QUE  diff = j-i  so we have taken i from start we are adding diff to i to find j 
+ // similarly acc to QUE  diff = k-j  so we have taken i from start we are adding 2*diff to i to find k
+        for(int i = 0 ;i<nums.length;i++){
+            
+           if(hashMap.containsKey(nums[i]+diff) && hashMap.containsKey(nums[i]+2*diff)){
+               count+=1;
+           }}
         return count ;
+            
+            
+        }
         
-    }
 }
