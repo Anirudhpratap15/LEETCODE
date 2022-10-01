b68053526ea25f774fa9bcb376afe5e3086f9046
Anirudh Pratap <singh.anirudh15@gmail.com>
2022-10-01 20:40:13 +0530

    Time: 79 ms (29.77%), Space: 64.3 MB (24.32%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,28 +1,51 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);									
+      Set<List> result = new HashSet<>();
 
-       Set<List> ans = new HashSet<>();
-        for(int i = 0 ; i <nums.length-2;i++){
-            int left  = i+1 ;
-            int right = nums.length-1;
-             
-            while (left < right) {
-           int sum = nums[i] + nums[left]+ nums[right]; 
-            if(sum ==0){
-                ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
-                while(left<right&&nums[left] == nums[left+1]) left++;
-                while(left<right&&nums[right] == nums[right-1]) right--;
+    if (nums == null || nums.length < 3)
+        return new ArrayList(result);
+    
+   // Sort the elements
+    Arrays.sort(nums);									
+
+    // taking first element and finding other two which have equal sum to the first element  
+    for (int i = 0; i <  nums.length - 2; i++)				
+    {
+       // using two pointer 
+// first pointer start from left -> i+1
+// second pointer start from last element
+//1 //as array is sorted if sum is less than ith element then increase the left pointer
+//2 //as array is sorted if sum is more than ith element then decrease the right pointer
+
+        int left = i + 1;
+		int right =  nums.length - 1;
+        
+        while (left < right)
+        {
+            int sum =  nums[i] + nums[left] + nums[right];
+            
+            if (sum == 0)
+            {
+                result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                
+                //when left pointer have dublicate value skip ++
+                  while(left<right&&nums[left]==nums[left+1]) left++;
+               
+                //when right pointer have dublicate value skip --
+                  while(left<right&&nums[right]==nums[right-1]) right--;
+
                 left++;
                 right--;
             }
-            else if(sum<0)left++;
-            else right--;
-                
-                
-                
-        }}
-        return new ArrayList(ans) ; 
+            //see point 1
+            else if(sum < 0)
+                left++;
+            // see point 2
+            else 													
+                right--;
+        }
+    }
+    return new ArrayList(result);
         
     }
 }
