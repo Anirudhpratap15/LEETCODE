1bce469d2aa356f1bdd3abc48c75d71a9f640dd4
Anirudh Pratap <singh.anirudh15@gmail.com>
2022-10-05 13:34:39 +0530

    Time: 6 ms (22.26%), Space: 57.7 MB (52.68%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,25 +1,27 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         
+        // taking array for output
         int[] ans = new int[nums.length];
         
+        //taking aray for product of nums from left eg -{1,1*1,2*1,3*2} => {1,1,2,6} 
         int[] leftProduct = new int[nums.length];
-          int[] rightProduct = new int[nums.length];
         
+        //as product of left from 0th index is not present taking 1 
          leftProduct[0] = 1 ;
-          rightProduct[nums.length-1] = 1 ;
         
+       // taking product of nums from left
         for(int i = 1 ;i < nums.length;i++){
             leftProduct[i] =  leftProduct[i-1]* nums[i-1];
         }
         
-          
-        for(int i = nums.length-2 ;i>=0;i--){
-            rightProduct[i] =  rightProduct[i+1]* nums[i+1];
-        }
-        
-          for(int i = 0 ;i < nums.length;i++){
-            ans[i] =  rightProduct[i]*  leftProduct[i];
+        int p = 1 ;
+        // multiply leftIndexProduct to right index product which is store in p ;
+          for(int i = nums.length-1 ;i>=0;i--){
+            
+            ans[i] =  p * leftProduct[i];
+                p=p*nums[i];
+              
         }
         return ans ;
         
