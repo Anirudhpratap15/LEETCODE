6949e71df5ffe9638a50de2de3d5e0b4aaa1d30b
Anirudh Pratap <singh.anirudh15@gmail.com>
2022-10-16 00:47:58 +0530

    Time: 2 ms (84.65%), Space: 57.5 MB (55.98%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,14 +1,18 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
       int i = 0 ;
+        //sorting using cycle sort as we want smallest postive number so ans is in the range  [1-n] if not then n+1 is the ans 
         while(i<nums.length){
             int correctIndex= nums[i]-1;
+            //ignoring negative number and number larger then length
             if(nums[i]>0&&nums[i]<=nums.length&&nums[i]!=nums[correctIndex]){
                 swap(i,correctIndex,nums);
             }
             else{i++;}
         }
         
+        
+        //find smallest  missing number 
         for(int j =0 ;j<nums.length;j++){
             if(nums[j]!=j+1){
                 return j+1;
