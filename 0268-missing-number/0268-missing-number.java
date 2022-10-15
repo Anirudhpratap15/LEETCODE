8ed0dfa2026062435ae37460edb3d2183c003be1
Anirudh Pratap <singh.anirudh15@gmail.com>
2022-10-15 13:34:31 +0530

    Time: 1 ms (85.38%), Space: 43.1 MB (89.60%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,21 +1,25 @@
 class Solution {
     public int missingNumber(int[] nums) {
    int i =0 ;
+        //using cycle sort  sorting based on index 0 - n  
         while(i<= nums.length-1){
+            // finding correct index of element at ith index  as index and element should be same bcoz range of array is [0,n]
             int correctIndex = nums[i];
+            //ignoring length(last element) element as  one  number  is missing so that one index is not present in array so  we cannot swap that last element with last index  
             if (nums[i]!= nums.length&&nums[i]!=nums[correctIndex]){
                 swap(i,correctIndex,nums);
             }
             else i++;
         }
         
+        // if index not match with element then index is the ans 
         for(int j = 0 ; j<nums.length;j++){
             if(j!=nums[j]){
                 return j;
             }
         }
 
-
+// if last number of array is missing 
         return nums.length;
         
     }
