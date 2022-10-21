f71c3f40750f2ce75b7398510af5cca776ec3dc5
Anirudh Pratap <singh.anirudh15@gmail.com>
2022-10-21 23:30:40 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (74.54%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,6 +1,17 @@
 class Solution {
     public String defangIPaddr(String address) {
-      return address.replace(".","[.]");
+        // string is immutable(not changing ) so we are using StringBuilder which is mutable 
+        final StringBuilder s = new StringBuilder();
+        
+        //traverse original string and storing it in new string s  if (.) encounters replace that with [.] 
+        for (int i = 0; i < address.length(); i++) {
+            if (address.charAt(i) == '.')
+                s.append("[.]");
+            else
+                s.append(address.charAt(i));
+        }
+        
+        return s.toString();
         
     }
 }
