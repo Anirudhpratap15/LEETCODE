43a86b11b8b6e2349e44693e56af17253bc5c846
Anirudh Pratap <singh.anirudh15@gmail.com>
2022-10-30 23:00:17 +0530

    Time: 7 ms (77.32%), Space: 44.8 MB (79.06%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int romanToInt(String s) {
+         int sum = 0;
+        int prev = 0;
+        // checking for romam using switch case
+        for (int i = s.length() - 1; i >= 0; i--)
+        {
+            int cur = 0;
+            switch (s.charAt(i))
+            {
+            case 'I':
+                cur = 1;
+                break;
+            case 'V':
+                cur = 5;
+                break;
+            case 'X':
+                cur = 10;
+                break;
+            case 'L':
+                cur = 50;
+                break;
+            case 'C':
+                cur = 100;
+                break;
+            case 'D':
+                cur = 500;
+                break;
+            case 'M':
+                cur = 1000;
+                break;
+            }
+            //if current is smaller then previous eg  IV - 4   current is I and previous is V 
+            if (cur < prev)
+            {
+                sum -= cur;
+            }
+            else
+            {
+                sum += cur;
+            }
+            prev = cur;
+        }
+        return sum;    
+      
+    }
+}
